8e85a354fb24eee64b0fc2fa26951cabb579ed9f
Kapil <kapilguptakg1519@gmail.com>
2025-12-22 11:49:44 +0530

    bot detection

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { signup, login, logout, updateProfile } from '../controllers/authControllers.js';
 import { protectRoute } from '../middlewares/authMiddleware.js';
+import { arcjetProtection } from '../middlewares/arcjetMiddleware.js';
 const router = express.Router();
 
-
+router.use(arcjetProtection);
 router.post("/signup", signup)
-router.post("/login", login);
+router.post("/login", arcjetProtection, login);
 router.post("/logout", logout);
 router.put("/update_profile", protectRoute, updateProfile);
 router.get("/check", protectRoute, (req,res) => res.status(200).json(req.user));
