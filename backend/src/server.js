b8ea372fa6f02e6eb81be3a2abbf6aa79540e5c2
Kapil <kapilguptakg1519@gmail.com>
2025-12-24 16:48:54 +0530

    update backend with socketio

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,15 +6,15 @@ import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json()); //req.body
+app.use(express.json({ limit: "15mb" })); //req.body
 app.use(cors({origin:ENV.CLIENT_URI, credentials:true}))
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -28,7 +28,7 @@ if(ENV.NODE_ENV === "production"){
     })
 }
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log("Server is running on the port: " + PORT)
     connectDB()
 })
